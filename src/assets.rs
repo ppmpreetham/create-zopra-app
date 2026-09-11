9aea65ddd4f0a2eb79a746ba0ed44dc449125e96
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-09-11 11:14:28 +0530

    fix: name confusion

--- src/assets.rs
+++ src/assets.rs
@@ -1,29 +1,29 @@
 use anyhow::Result;
-use gpui_kit::{AssetSource, SharedString};
+use gpui_kit::{AssetSource, SharedString, assets::Assets};
 use rust_embed::RustEmbed;
 use std::borrow::Cow;
 
 #[derive(RustEmbed)]
 #[folder = "assets"]
-pub struct Assets;
+pub struct AppAssets;
 
 // TODO: make zopra do this later
-impl AssetSource for Assets {
+impl AssetSource for AppAssets {
     fn load(&self, path: &str) -> Result<Option<Cow<'static, [u8]>>> {
-        if let Some(file) = Assets::get(path) {
+        if let Some(file) = AppAssets::get(path) {
             return Ok(Some(file.data));
         }
 
-        gpui_kit::assets::Assets.load(path)
+        Assets.load(path)
     }
 
     fn list(&self, path: &str) -> Result<Vec<SharedString>> {
-        let mut files = Assets::iter()
+        let mut files = AppAssets::iter()
             .filter(|file| file.starts_with(path))
             .map(SharedString::from)
             .collect::<Vec<_>>();
 
-        files.extend(gpui_kit::assets::Assets.list(path)?);
+        files.extend(Assets.list(path)?);
 
         Ok(files)
     }
